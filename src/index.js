705ea5fa2e41b81f8684afc9c9eea56cd6385227
Charles NDAYISABA <nccharles1@gmail.com>
2021-06-22 02:18:51 +0200

    redux persist

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
 import { configureStore } from "./store";
-
+const store = configureStore();
+const persiststor = persistStore(store);
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<div>Loading...</div>} persistor={persiststor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
